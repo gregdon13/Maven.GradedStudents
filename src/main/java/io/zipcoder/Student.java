25204dfe16ac26940910b48d100615f771245253
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-10 22:10:31 -0500

    getExamScores WORKS

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -2,6 +2,8 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Student {
     String firstName = "";
@@ -11,8 +13,50 @@ public class Student {
     public Student(String firstName, String lastName, Double[] testScores) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.examScores = (ArrayList<Double>) Arrays.asList(testScores);
+        List<Double> examList = new ArrayList<>();
+        Collections.addAll(examList, testScores);
+        this.examScores = (ArrayList<Double>) examList;
     }
 
-    
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String  getLastName() {
+        return this.lastName;
+    }
+
+    public String getExamScores() {
+        String output = "Exam Scores:\n";
+        Double[] examArrDbl = examScores.toArray(new Double[examScores.size()]);
+        String[] examStrArr = new String[examArrDbl.length];
+        for (int i = 0; i <= examStrArr.length - 1; i++) {
+            examStrArr[i] = examArrDbl[i].toString();
+            if (i == examArrDbl.length - 1) {
+                output += ("Exam " + (i+1) + ": " + examStrArr[i]);
+            } else {
+                output += ("Exam " + (i + 1) + ": " + examStrArr[i] + "\n");
+            }
+        }
+        //output += ("Exam " + (examStrArr.length) + ": " + examStrArr[examStrArr.length-1]);
+        System.out.println(output);
+        return output;
+    }
+
+    public void setExamScores(Double[] examScores) {
+        this.examScores = (ArrayList<Double>) Arrays.asList(examScores);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,29 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void constructorTest() {
+
+    }
+
+    @Test
+    public void getExamScoresTest() {
+        //Given
+        String firstName = "Darrow";
+        String lastName = "Andromedus";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        //When
+        String output = student.getExamScores();
+        String expected = "Exam Scores:\n" + "Exam 1: 100.0\n" + "Exam 2: 95.0\n" + "Exam 3: 123.0\n" + "Exam 4: 96.0";
+
+        //Then
+        Assert.assertEquals(expected, output);
+    }
 
 }
