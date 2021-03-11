1ff81835901675ea2981d45ecf2c9362bf5013df
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-11 16:01:27 -0500

    Getters/setters for classroom made

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
+
 public class Classroom {
+    Student[] students;
+
+    public Classroom (int maxNumberOfStudents) {
+
+    }
+
+    public Classroom(Student[] students) {
+
+    }
+
+    public Classroom () {
+        Student[] students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    //PH return
+    public double getAverageExamScore() {
+        //Student studentPH = new Student();
+        double sumOfAverages = 0.0;
+        for (int i = 0; i < students.length; i++) {
+
+        }
+        return 5.0;
+    }
 }
