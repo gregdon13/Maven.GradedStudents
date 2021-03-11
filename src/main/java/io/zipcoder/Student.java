7332a60a15fbe11ef9df0ef745fb5f7f64714129
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-10 20:51:51 -0500

    started working on it

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,18 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    String firstName = "";
+    String lastName = "";
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = (ArrayList<Double>) Arrays.asList(testScores);
+    }
+
+    
 }
